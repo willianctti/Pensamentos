d2be0048643c44f4428c1d4e82dd596780528f55
willianctti <willianctti@gmail.com>
2023-06-12 15:46:18 -0300

    finalizando projeto

--- controllers/PensamentoController.js
+++ controllers/PensamentoController.js
@@ -1,9 +1,39 @@
 const Pensamento = require('../models/Pensamento')
 const User = require('../models/User')
 
+const { Op} = require('sequelize')
+
 module.exports = class PensamentoController {
     static async showPensamentos(req, res) {
-        res.render('pensamentos/home')
+        let search = ''
+        if(req.query.search) {
+            search = req.query.search
+        }
+
+        let order = 'DESC'
+        if(req.query.order === 'old') {
+            order = 'ASC'
+        } else {
+            order = 'DESC'
+        }
+
+
+        const pensamentosData = await Pensamento.findAll({
+            include: User,
+            where: {
+                title: {[Op.like]: `%${search}%`},
+            },
+            ORDER: [['createdAt', order]]
+        })
+        const pensamentos = pensamentosData.map((result) => result.get({plain: true}))
+
+        let pensamentosQty = pensamentos.length
+        if(pensamentosQty=== 0) {
+            pensamentosQty = false
+        }
+
+
+        res.render('pensamentos/home', {pensamentos, search, pensamentosQty})
     }
 
     static async dashboard(req, res) {
@@ -21,7 +51,11 @@ module.exports = class PensamentoController {
         }
         const pensamentos = user.Pensamentos.map((result) => result.dataValues)
 
-
+        let emptyPensamentos = false
+        if(pensamentos.length === 0) {
+            emptyPensamentos = true
+            
+        }
 
 
         res.render('pensamentos/dashboard', { pensamentos})
@@ -48,6 +82,49 @@ module.exports = class PensamentoController {
                 res.redirect('/pensamentos/dashboard')
            })
 
+        } catch(error) {
+            console.log(error)
+        }
+    }
+
+    static async removePensamento(req, res) {
+        const id = req.body.id
+        const UserId = req.session.userid
+
+        try {
+            await Pensamento.destroy({where: {id: id, UserId: UserId}})
+            req.flash('message', 'Pensamento removido com sucesso!')
+
+            req.session.save(() => {
+                res.redirect('/pensamentos/dashboard')
+            })
+        } catch(error) {
+            console.log(error)
+        }
+    }
+
+    static async updatePensamento(req, res) {
+        const id = req.params.id
+
+        const pensamento = await Pensamento.findOne({where: {id: id}, raw: true})
+
+        res.render('pensamentos/edit', { pensamento})
+    }
+
+    static async updatePensamentoSave(req, res) {
+
+        const id = req.body.id
+        const pensamento  = {
+            title: req.body.title
+        }
+
+        try {
+            await Pensamento.update(pensamento, {where: {id: id}})
+            req.flash('message', 'Pensamento alterado com sucesso!')
+    
+            req.session.save(() => {
+                res.redirect('/pensamentos/dashboard')
+            })
         } catch(error) {
             console.log(error)
         }
--- routes/pensamentosRoutes.js
+++ routes/pensamentosRoutes.js
@@ -9,10 +9,11 @@
 
  router.get('/add', checkAuth, PensamentoController.createPensamento)
  router.post('/add', checkAuth, PensamentoController.createPensamentoSave)
+ router.get('/edit/:id', checkAuth, PensamentoController.updatePensamento)
+ router.post('/edit', checkAuth, PensamentoController.updatePensamentoSave)
 
-
+ router.post('/remove', checkAuth, PensamentoController.removePensamento)
  router.get('/dashboard', checkAuth, PensamentoController.dashboard)
-
  router.get('/', PensamentoController.showPensamentos)
 
  module.exports = router
